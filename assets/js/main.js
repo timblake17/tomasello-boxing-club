164ce269598636a07e5fc6fc2f3ba11a34d92503
timBlake <timblake@resilient-coders-11s-MacBook-Air.local>
2017-11-21 12:33:23 -0500

    home page good waiting for review

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
 (function() {
     console.log("Javascript connected");
 })();
+
+
+$(".menu-toggle").on("click",function(){
+  $(".site-nav").toggleClass("siteNavOpen",500);
+  $(this).toggleClass("open");
+
+});
